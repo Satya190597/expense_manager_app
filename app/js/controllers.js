39f5fc805150860f9cbcd95029aef6426611e356
satya190597 <webprakashsatya@gmail.com>
2019-09-05 15:38:28 +0530

    Added Empty Home Controller & Partial View

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,11 +1,12 @@
 'use strict';
 
-/* Controllers */
-
 angular.module('myApp.controllers', [])
   .controller('MyCtrl1', ['$scope', function($scope) {
 
   }])
   .controller('MyCtrl2', ['$scope', function($scope) {
 
-  }]);
+  }])
+  .controller('HomeCtrl',['$scope',function($scope){
+
+  }])
